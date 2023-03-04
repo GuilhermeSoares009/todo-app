8a2c071b90e823c7119484fee717293d62f15f41
Guilherme <guilherme.sousa009@hotmail.com>
2023-03-03 21:35:11 -0300

    Segunda Parte da Ambientação - Tirando multi-word-component-names - Criando componente do header (apenas arquivo) e adicionando o index. - Começo bem simples.

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <main id="todo" class=".container">
+    <div id="todo_cabecalho" class=".col-md-8">
+    </div>
+    <Cabecalho />
+  </main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
+import { defineComponent } from 'vue';
+import Cabecalho from './components/CabecalhoTodo.vue'
+export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    Cabecalho
   }
-}
+});
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
